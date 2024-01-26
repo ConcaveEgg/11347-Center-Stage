d97a1a23ba55d4c12d1ce5b16815250c904f508b
Akshaj Y <Akshajy6@gmail.com>
2024-01-26 18:24:46 -0500

    Update BlueClose.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueClose.java
@@ -5,13 +5,17 @@ import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.subsystems.Outtake;
 import org.firstinspires.ftc.teamcode.subsystems.Slides;
 import org.firstinspires.ftc.teamcode.subsystems.V4B;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+import java.util.concurrent.TimeUnit;
 
 
 @Autonomous(name="Blue Close")
@@ -23,7 +27,11 @@ public class BlueClose extends CommandOpMode {
     V4B v4b;
     GamepadEx gamepad;
 
+    private final int READ_PERIOD = 1;
+    private HuskyLens huskyLens;
+
     public static Pose2d startPoseCloseBlue = new Pose2d(12,62, Math.toRadians(-90));
+    String section;
 
     @Override
     public void initialize() {
@@ -31,26 +39,65 @@ public class BlueClose extends CommandOpMode {
         s = new Slides(hardwareMap);
         o = new Outtake(gamepad, hardwareMap);
         v4b = new V4B(hardwareMap);
+        huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
 
+        Deadline rateLimit = new Deadline(READ_PERIOD, TimeUnit.SECONDS);
+        rateLimit.expire();
 
-        while (!isStarted() && !isStopRequested()) {
-            telemetry.addLine("Waiting For Start...");
+        if (!huskyLens.knock()) {
+            telemetry.addData(">>", "Problem communicating with " + huskyLens.getDeviceName());
+            telemetry.update();
         }
+        huskyLens.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
+
+        TrajectorySequence propCloseLeftBlue = drive.trajectorySequenceBuilder(startPoseCloseBlue)
+                .forward(25)
+                .turn(Math.toRadians(90))
+                .forward(11)
+                .back(11)
+                .strafeLeft(25)
+                .build();
+
+        TrajectorySequence scoreCloseLeftBlue = drive.trajectorySequenceBuilder(propCloseLeftBlue.end())
+                .forward(34)
+                .strafeRight(12)
+                .build();
+
+        TrajectorySequence parkCloseLeftBlue = drive.trajectorySequenceBuilder(scoreCloseLeftBlue.end())
+                .strafeLeft(12)
+                .forward(12)
+                .build();
 
         TrajectorySequence propCloseMidBlue = drive.trajectorySequenceBuilder(startPoseCloseBlue)
                 .forward(25)
                 .lineToLinearHeading(new Pose2d(12, 62, 0))
                 .build();
-
         TrajectorySequence scoreCloseMidBlue = drive.trajectorySequenceBuilder(propCloseMidBlue.end())
                 .forward(34)
                 .strafeRight(19)
                 .build();
-
         TrajectorySequence parkCloseMidBlue = drive.trajectorySequenceBuilder(scoreCloseMidBlue.end())
                 .strafeLeft(25)
                 .forward(12)
                 .build();
+
+        TrajectorySequence propCloseRightBlue = drive.trajectorySequenceBuilder(startPoseCloseBlue)
+                .forward(25)
+                .turn(Math.toRadians(-90))
+                .forward(11)
+                .back(11)
+                .lineToLinearHeading(new Pose2d(12, 62, 0))
+                .build();
+
+        TrajectorySequence scoreCloseRightBlue = drive.trajectorySequenceBuilder(propCloseRightBlue.end())
+                .forward(34)
+                .strafeRight(12)
+                .build();
+
+        TrajectorySequence parkCloseRightBlue = drive.trajectorySequenceBuilder(scoreCloseRightBlue.end())
+                .strafeLeft(24)
+                .forward(12)
+                .build();;
 
 //        schedule(new SequentialCommandGroup ( //Makes the following code run one after another, like norma
 //            new ParallelCommandGroup(
